c6942b8c5de20b8ba6f3c881ce4c17a0dd8e2bd4
Pignataro67 <Pignataro67@gmail.com>
2020-03-05 09:54:50 -0800

    adding controller

--- Gemfile
+++ Gemfile
@@ -27,6 +27,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'foreman', '~> 0.82.0'
+
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'rest-client'
+
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+class SearchController < ApplicationController
+
+  def search_start
+    input = params[:input]
+    response = RestClient::Request.execute(
+      method: :get,
+      url: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{input}&key=#{ENV['GOOGLE_PLACES_KEY']}",
+      )
+    render json: response
+  end
+  
+  def search_destination
+  
+  end
+end
